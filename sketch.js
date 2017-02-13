ba4a0449abb698c10d0feb6c52b829bc22db942c
sukisukisuki <sukisekula@gmail.com>
2017-02-13 13:38:36 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,45 @@
 function setup() {
-
-}
-
-function draw() {
-
+    //suki sekula, ss5951@bard.edu, 2/12/17, my avatar, a horrifying ghost which lives in the computer. This was sort of difficult to learn how to make, especially since I  had a difficult time starting a new "empty idea" project. So it took me a few hours to get the live preview set up. 
+createCanvas(500, 500);
+    background(204);
+    strokeWeight(0);
+    fill(255, 255, 153);
+    rect(213, 80, 75, 100);
+    fill(255, 229, 204);
+    ellipse(250, 100, 60, 80); 
+    fill(255, 255, 153);
+    rect(213, 59, 75, 22);
+    fill(255, 255, 255);
+    ellipse(235, 100, 10, 6);
+    ellipse(265, 100, 10, 6);
+    fill(52, 102, 0);
+    ellipse (235, 100, 5, 3);
+    ellipse(265, 100, 5, 3);
+    fill(0);
+    ellipse (235, 100, 2, 1);
+    ellipse(265, 100, 2, 1);
+    fill(204, 0, 0);
+    ellipse (250, 124, 20, 15);
+    fill(250, 250, 250);
+    ellipse(245, 127, 3, 3);
+    ellipse(248, 127, 3, 3);
+    ellipse(251, 127, 3, 3);
+    ellipse(254, 127, 3, 3);
+    ellipse(245, 120, 3, 3);
+    ellipse(247.75, 120, 3, 3);
+    ellipse(251.3, 120, 3, 3);
+    ellipse(254, 120, 3, 3);
+    fill(0);
+    ellipse(248, 110, 0.8, 0.7);
+    ellipse(252, 110, 0.8, 0.7);
+    fill(102, 51, 0);
+    ellipse(230, 115, 0.5, 0.5);
+    ellipse(233, 110, 0.5, 0.5);
+    ellipse(265, 115, 0.5, 0.5);
+    ellipse(267, 119, 0.5, 0.5);
+    ellipse(271, 107, 0.5, 0.5);
+    fill(0);
+    textSize(3);
+    text("sukisukisukisukisukisukisukisukisukisukisukisukisukisuki",213,180);
+    
 }
